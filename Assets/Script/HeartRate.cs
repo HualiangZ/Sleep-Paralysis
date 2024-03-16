13dd71c15da95bfd65003d41335cde9b14b8c58e
HualiangZ <zhualiang88@gmail.com>
2024-03-16 14:51:32 +0000

    heart rate now working

--- Assets/Script/HeartRate.cs
+++ Assets/Script/HeartRate.cs
@@ -8,6 +8,9 @@ public class HeartRate : MonoBehaviour
     // Start is called before the first frame update
     public TMP_Text heart;
     private int abnormalCount = 0;
+    private int defultHeartRate = 60;
+    private int maxHeartRate = 60;
+    private int currentHeartRate = 60;
     GameObject[] allObjects;
     private void Awake()
     {
@@ -16,7 +19,9 @@ public class HeartRate : MonoBehaviour
     void Start()
     {
         StartCoroutine(Check());
-        
+        StartCoroutine(HR());
+        StartCoroutine(UpdateCurrentHR());
+
     }
 
     // Update is called once per frame
@@ -41,6 +46,38 @@ public class HeartRate : MonoBehaviour
             yield return new WaitForSeconds(2f);
         }
 
+    }
+
+    IEnumerator HR()
+    {
+        for(; ; )
+        {
+            //currentHeartRate = 60 + Random.Range(-2,2);
+            maxHeartRate = defultHeartRate * (abnormalCount+1);
+            //heart.text = count.ToString();
+            if (currentHeartRate < maxHeartRate)
+            {
+                currentHeartRate += 3;
+            }
+            else if(currentHeartRate > maxHeartRate)
+            {
+                currentHeartRate -= 3;
+            }
+            else
+            {
+                currentHeartRate = maxHeartRate;
+            }
+            yield return new WaitForSeconds(2f); 
+        }
+    }
+
+    IEnumerator UpdateCurrentHR()
+    {
+        for(; ; )
+        {
+            heart.text = (currentHeartRate + Random.Range(-2, 2)).ToString();
+            yield return new WaitForSeconds(1f);
+        }
     }
 
 }
